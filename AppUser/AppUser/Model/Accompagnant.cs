8be412f8396eb2e078db6070dec04c2b8009b232
hedimami <hedimami0@gmail.com>
2017-02-09 12:52:11 -0700

    Create Interface : connexion - visit - accomapagnant Create Models :visit - accomapagnant - favori-utilisateur

---
+++ AppUser/AppUser/Model/Accompagnant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUser.Model
+{
+   public class Accompagnant
+    {
+        public string nom { get; set; }
+        public int age { get; set; }
+        public String sexe { get; set; }
+        public String Idutilisateur { get; set; }
+        public String Idvisit { get; set; }
+    }
+}
---
+++ AppUser/AppUser/Model/Visit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUser.Model
+{
+   public class Visit
+    {
+        public String Id  { get; set; }
+        public DateTime datedepart { get; set; }
+        public DateTime datefin { get; set; }
+        public Double budget { get; set; }
+        public String idUtilisateur { get; set; }
+    }
+}
